c323668c19f64d8a785d650c49195d8bbf307b8d
unknown <ezichifavour@gmail.com>
2021-11-08 17:49:59 -0800

    first commit

---
+++ src/Pages/Contact-us/index.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Footer from '../../Components/Footer';
+import contactimg from '../../store/contactimg.svg';
+import './index.css';
+
+function ContactUs() {
+    const history = useHistory();
+
+    const initialValues = {
+        FullName: "",
+        Email: "",
+        Message: ""
+    };
+
+    const [values, setValues] = useState(initialValues);
+    // handle change function
+    const handleInputChange = (e) => {
+
+        const { name, value } = e.target;
+        e.preventDefault();
+        setValues({
+            ...values,
+            [name]: value,
+        });
+        console.log(values);
+    };
+    function handleInputSubmission() {
+        history.push('/');
+    }
+
+    return (
+        <div id="contact-us-section">
+
+            <img className="contact-us-image" src={contactimg} alt="" />
+
+
+            <form className="contact-us-form" onSubmit={handleInputSubmission}>
+                <h1>Get in touch for more enquiries</h1>
+
+                <p>Leave us a message, we will get back to you shortly</p>
+
+                <label htmlFor="">Full Name</label>
+                <input
+                    className="contact-us-inputs"
+                    type="text"
+                    name="FullName"
+                    label="FullName"
+                    value={values.FullName}
+                    onChange={handleInputChange}
+                    required />
+
+
+                <label htmlFor="">Email Address</label>
+                <input
+                    className="contact-us-inputs"
+                    type="text"
+                    name="Email"
+                    label="Email"
+                    value={values.Email}
+                    onChange={handleInputChange}
+                    required />
+
+
+                <label htmlFor="">Message</label>
+                <input
+                    className="contact-us-input3"
+                    type="text"
+                    name="Message"
+                    label="Message"
+                    value={values.Message}
+                    onChange={handleInputChange}
+                    required />
+
+                <input id="contact-us-btn" type="submit" value="Send" />
+
+            </form>
+
+            <Footer />
+        </div>
+
+    );
+}
+export default ContactUs;
